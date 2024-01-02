7c963125082b74ed02a415bfb746c5a4add03d45
Amir <34164@ma-web.nl>
2024-01-02 13:01:56 +0100

    new version 03 ifelse

--- 03 ifelse/app.js
+++ 03 ifelse/app.js
@@ -6,16 +6,42 @@ class App
         console.log("hello world!");
         //code gaat hier!
         // Selecteer het element met de id "newstitle"
-        let nieuwsTitelElement = document.getElementById("newstitle");
-        console.log("Element met ID 'newstitle':", nieuwsTitelElement);
+
+        
+        let title = document.getElementById("newstitle");
+        console.log("Title element:", title);
+
+        let random = Math.random();
+        console.log(random);
+        if(random < 0.2)
+        {
+            title.style.background = "#FF0000";
+        }
 
         // Selecteer het element met de class "gamenews" en "headline"
-        let gameNewsElement = document.querySelector(".gamenews.headline");
-        console.log("Element met class 'gamenews' en 'headline':", gameNewsElement);
+        let newsitem1 = document.getElementsByClassName("gamenews")[0];
+        console.log("Newsitem 1 (Mario Kart):", newsitem1);
 
         // Selecteer het element met de class "fromsoftware"
-        let fromSoftwareElement = document.querySelector(".fromsoftware");
-        console.log("Element met class 'fromsoftware':", fromSoftwareElement);
+        let newsitem2 = document.getElementsByClassName("gamenews")[1];
+        console.log("Newsitem 2 (From Software):", newsitem2);
+
+        if (title) {
+            title.style.background = "red"
+        
+        }
+
+        if (newsitem1) {
+            newsitem1.style.backgroundColor = "lightgreen";
+        } else {
+            console.error("Newsitem 1 niet gevonden!");
+        }
+
+        if (newsitem2) {
+            newsitem2.style.backgroundColor = "lightcoral";
+        } else {
+            console.error("Newsitem 2 niet gevonden!");
+        }
 
     }
 }
