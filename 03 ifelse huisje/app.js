0b4589e4fe4cc7d24d7cd503efa56ab3e26e8878
Amir <34164@ma-web.nl>
2024-01-12 09:27:58 +0100

    03 ifelse huisje

---
+++ 03 ifelse huisje/app.js
@@ -0,0 +1,118 @@
+class App 
+{
+    runApplication()
+    {
+
+        
+    //line
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+let canvas = document.getElementById("Canvasid");
+let g = canvas.getContext("2d");
+
+
+
+
+let random = Math.random();
+console.log(random);
+
+
+  
+
+ g.fillStyle = "red";
+ g.beginPath();
+ g.moveTo(30,10);
+ g.lineTo(20,30);
+ g.lineTo(60,50);
+ g.lineTo(70,20);
+ g.closePath();
+ g.stroke();
+ g.fill(); 
+
+ g.fillStyle = "gray";
+ g.beginPath();
+ g.moveTo(20,30);
+ g.lineTo(20,50);
+ g.lineTo(60,60);
+ g.lineTo(60,40);
+ g.closePath();
+ g.stroke();
+ g.fill();
+
+ g.fillStyle = "gray";
+ g.beginPath();
+ g.moveTo(70,20);
+ g.lineTo(60,40);
+ g.lineTo(80,30);
+  g.closePath();
+ g.stroke();
+ g.fill();
+
+ 
+ g.fillStyle = "gray";
+ g.beginPath();
+ g.moveTo(60,40);
+ g.lineTo(60,60);
+ g.lineTo(80,50);
+ g.lineTo(80,30);
+ g.closePath();
+ g.stroke();
+ g.fill();
+
+ g.fillStyle = "lightblue";
+ g.beginPath();
+ g.moveTo(30,40);
+ g.lineTo(30,40);
+ g.lineTo(40,40);
+ g.lineTo(40,45);
+ g.lineTo(30,45);
+ g.closePath();
+ g.stroke();
+ g.fill();
+
+ g.fillStyle = "yellow";
+ g.beginPath();
+ g.moveTo(30,40);
+ g.lineTo(30,40);
+ g.lineTo(40,40);
+ g.lineTo(40,45);
+ g.lineTo(30,45);
+ g.closePath();
+ g.stroke();
+ g.fill();
+
+if(random > 0.2&& random < 0.6){
+g.fillStyle = "green";
+}else if(random > 0.6){
+    g.fillStyle = "black";
+}
+
+ g.beginPath();
+ g.moveTo(30,40);
+ g.lineTo(30,40);
+ g.lineTo(40,40);
+ g.lineTo(40,45);
+ g.lineTo(30,45);
+ g.closePath();
+ g.stroke();
+ g.fill();
+
+ let randomGetal = Math.random(); 
+ if (randomGetal > 0.5) {
+    drawWindow(true);
+ } else{
+    drawWindow(false);
+ }
+
+ 
+        
+
+            
+        
+
+    
+
+    }
+}
+
+let app = new App();
+app.runApplication();
